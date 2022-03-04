9804d60c662ba0d32e1cb544e687eb3e60e87cde
gafencu-beniamin-30122 <benigafencu@yahoo.com>
2022-03-04 18:01:13 +0200

    First projects in Python

---
+++ Hangman_animals.py
@@ -0,0 +1,44 @@
+import random
+def hangman(word):
+    wrong = 0
+    stages = ["",
+             "________        ",
+             "|               ",
+             "|        |      ",
+             "|        0      ",
+             "|       /|\     ",
+             "|       / \     ",
+             "|               "
+              ]
+    rletters = list(word)
+    board = ["__"] * len(word)
+    win = False
+    print("Welcome to Hangman")
+    print(" ".join(board))
+    while wrong < len(stages) - 1:
+        print("\n")
+        msg = "Guess a letter: "
+        char = input(msg)
+        if char in rletters:
+            cind = rletters.index(char)
+            board[cind] = char
+            rletters[cind] = '$'
+        else:
+            wrong += 1
+        print((" ".join(board)))
+        e = wrong + 1
+        print("\n".join(stages[0: e]))
+        if "__" not in board:
+            print("You win!")
+            print(" ".join(board))
+            win = True
+            break
+    if not win:
+        print("\n"
+              .join(stages[0: wrong]))
+        print("You lose! It was {}.".format(word))
+
+word_list=["cat","dog","duck","horse","cow","mouse","chicken"]
+word_number=random.randint(0,7)
+word=word_list[word_number]
+hangman(word)
